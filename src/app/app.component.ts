575f3dd7676e2ce3549fdcf22f622f35c6ea4989
Matt Callahan <mattcllhn@gmail.com>
2017-02-07 10:34:12 -0600

    routing finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 // import {HeroesComponent} from "./heroes2.component.ts";
 
 @Component({
+  moduleId:module.id,
   selector: 'my-app',
   template:`
   <nav>
     <h1>{{title}}</h1>
-    <a routerLink = "/heroes">Heroes</a>
-    <a routerLink = "/">Dashboard</a>
+    <a routerLink = "/heroes" routerLinkActive = "active">Heroes</a>
+    <a routerLink  = "/" routerLinkActive = "active">Dashboard</a>
   </nav>
   <router-outlet></router-outlet>
-  `
+  `,
+  styleUrls:['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
